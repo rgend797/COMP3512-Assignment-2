c908591b0aae870c5e557007c0f81f7e48f83da2
katon1 <54992148+katon1@users.noreply.github.com>
2022-12-05 14:27:15 -0700

    Add files via upload

--- COMP3512-Assignment-2-main/files/assign2.js
+++ COMP3512-Assignment-2-main/files/assign2.js
@@ -7,7 +7,7 @@ const api = 'https://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.
 
 let getFile = [];   
 
-if(localStorage.length != 0){
+if(localStorage){
     getFile = localStorage.getItem('store');
     
 } else {
